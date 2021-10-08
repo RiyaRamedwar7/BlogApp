71ace893c6567f6563dd83570451ca99e5630a2b
RiyaRamedwar7 <82210688+RiyaRamedwar7@users.noreply.github.com>
2021-10-08 16:45:49 +0530

    Add files via upload

---
+++ src/Components/Navbar.js
@@ -0,0 +1,72 @@
+import {Avatar} from "@material-ui/core";
+import React, {useState} from "react";
+import { GoogleLogout } from "react-google-login";
+import {useDispatch, useSelector} from "react-redux";
+import {
+    selectSignedIn, 
+    selectUserData,
+    setInput,
+    setSignedIn,
+    setUserData,
+} from "../features/userSlice";
+
+import "../styling/navbar.css";
+
+const Navbar = () => {
+    const [inputValue, setInputValue] = useState("tech");
+    const isSignedIn = useSelector(selectSignedIn);
+    const userData = useSelector(selectUserData);
+
+    const dispatch = useDispatch();
+
+    const logout = (response) => {
+    dispatch(setSignedIn(false));
+        dispatch(setUserData(null));
+    };
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        dispatch(setInput(inputValue));
+      };
+    
+    
+    return (
+        <div className="navbar">
+            <h1 className="navbar__header">BlogMania</h1>
+            {isSignedIn && (
+              <div className="blog__search">
+                <input className="search"
+                placeholder="Search for a blog" 
+                value={inputValue} 
+                onChange={(e) => setInputValue(e.target.value)}/>
+                <button className="submit" onClick={handleClick}>Search</button> 
+             </div> 
+            )}
+
+            {isSignedIn ? (
+              <div className="navbar__user__data">
+                <Avatar 
+                className="user" 
+                src={userData?.imageURL} 
+                alt={userData?.name}/>
+                <h1 className="signedIn">{userData?.givenName}</h1>
+                <GoogleLogout clientId="1038320062331-1mvqd64vm5v6pov5rsse2sagsgeik4ju.apps.googleusercontent.com"
+                render={(renderProps) => (
+                 <button 
+                 onClick={renderProps.onClick}
+                 disabled={renderProps.disabled}
+                 className="logout__button">
+                     Logout
+                 </button>
+                 )} 
+                  onLogoutSuccess={logout}
+              />  
+            </div>
+    ) : ( 
+        ""
+    )}
+         </div>
+    );
+};
+
+export default Navbar;
